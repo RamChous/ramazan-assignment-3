e293391e546a7037a77e06b9b649c1f5b0ed1d6b
Ramazan <rahmettularamazan@gmail.com>
2025-04-26 21:13:58 +0500

    implementing put and ensureLoadFactor methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        MyHashTable<String, Integer> table = new MyHashTable<>();
+
+        table.put("Ramazan", 18);
+        table.put("Alihan", 11);
+        table.put("Anelya", 6);
+        table.put("Arlan", 19);
+        table.put("Anuar", 14);
+
+        System.out.println(table.size());
     }
 }
--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -15,6 +15,7 @@ public class MyHashTable<K, V> {
         }
     }
     private HashNode<K, V>[] chainArray;
+    private double loadFactor = 0.9;
     private int size;
 
     public MyHashTable(){
@@ -27,11 +28,45 @@ public class MyHashTable<K, V> {
         size = 0;
     }
 
-    private int hash(K key){
-        return 0;
+    private int hash(K key) {
+        return (key.hashCode() & 0x7fffffff) % chainArray.length;
     }
 
-    public void put(K key, V value){
+    public void ensureLoadFactor() {
+        if ((double) size / chainArray.length > loadFactor) {
+            HashNode<K, V>[] oldChainArray = chainArray;
+            chainArray = new HashNode[chainArray.length * 2];
 
+            for (HashNode<K, V> current : chainArray){
+                while (current != null) {
+                    HashNode<K, V> next = current.next;
+                    int index = hash(current.key);
+                    current.next = chainArray[index];
+                    chainArray[index] = current;
+                    current = next;
+                }
+            }
+        }
+    }
+
+    public void put(K key, V value) {
+        ensureLoadFactor();
+        int index = hash(key);
+        HashNode<K, V> current = chainArray[index];
+        while (current != null) {
+            if (current.key.equals(key)){
+                current.value = value;
+                return;
+            }
+            current = current.next;
+        }
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = chainArray[index];
+        chainArray[index] = newNode;
+        size++;
+    }
+
+    public int size(){
+        return size;
     }
 }

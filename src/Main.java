e8d0a97984e601342ee35c364e3b2373dbb04b99
Ramazan <rahmettularamazan@gmail.com>
2025-04-27 22:27:11 +0500

    small check for proper functionality.

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,44 @@ public class Main {
         table.put("Arlan", 19);
         table.put("Anuar", 14);
 
-        System.out.println(table.size());
+        System.out.println("Size: " + table.size());
+        System.out.println("Age of Ramazan: " + table.get("Ramazan"));
+        System.out.println("Age of Alihan: " + table.get("Alihan"));
+        System.out.println("Remove: " + table.remove("Arlan"));
+        System.out.println("Size after removing: " + table.size());
+        System.out.println("Table contains 6: " + table.contains(6));
+        System.out.println("Table contains 100" + table.contains(100));
+        System.out.println("Key of value 14 " + table.getKey(14));
+        System.out.println("Key of value 52 " + table.getKey(52));
 
-        System.out.println("//////////");
+        System.out.println("---------------------------");
 
-        System.out.println(table.get("Ramazan"));
-        System.out.println(table.get("Alihan"));
+        BST<Integer, String> bst = new BST<>();
 
-        System.out.println("//////////");
+        bst.put(23, "Root");
+        bst.put(11, "Left child");
+        bst.put(52, "Right child");
+        bst.put(7, "Left-Left child");
+        bst.put(22, "Left-Right child");
+        bst.put(99, "Right-Right child");
 
-        System.out.println(table.remove("Arlan"));
-        System.out.println(table.size());
+        System.out.println("Size: " + bst.size());
 
-        System.out.println("//////////");
+        System.out.println("Get 23 (Root): " + bst.get(23));
+        System.out.println("Get 11 (Left child): " + bst.get(11));
+        System.out.println("Get 52 (Right child): " + bst.get(52));
+        System.out.println("Get 7 (Left-Left child): " + bst.get(7));
+        System.out.println("Get 22 (Left-Right child): " + bst.get(22));
+        System.out.println("Get 99 (Right-Right child): " + bst.get(99));
 
-        System.out.println(table.contains(6));
-        System.out.println(table.contains(100));
+        System.out.println("Get 100 (Should be null): " + bst.get(100));
 
-        System.out.println("//////////");
+        bst.delete(23);
+        System.out.println("Get 23 (root): " + bst.get(23));
+        System.out.println("Size after removing: " + bst.size());
 
-        System.out.println(table.getKey(14));
-        System.out.println(table.getKey(52));
+        for (BST.Entry<Integer, String> e : bst.iterator()) {
+            System.out.println("Key: " + e.getKey() + " and value: " +  e.getValue());
+        }
     }
 }
